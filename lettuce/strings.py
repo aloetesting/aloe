ac31952427bc1c5434679f5cf7ad99bd94019a34
Danielle Madeley <danielle@madeley.id.au>
2014-02-09 03:04:51 +1100

    Remove a lot of dead code from lettuce.strings
    Approximately 98% of this has been replaced by lettuce.parsing, or is
    actually built into Python.

--- lettuce/strings.py
+++ lettuce/strings.py
@@ -27,249 +27,26 @@ def utf8_string(s):
     return s
 
 
-def escape_if_necessary(what):
-    what = unicode(what)
-    if len(what) is 1:
-        what = u"[%s]" % what
+def represent_table(table, indent=0):
+    """
+    Render a table
+    """
 
-    return what
+    if not table:
+        return ''
 
+    # calculate the width of each column
+    table = [[unicode(cell).replace('|', r'\|')
+              for cell in row]
+             for row in table]
+    lengths = [len(cell) for cell in table[0]]
 
-def get_stripped_lines(string, ignore_lines_starting_with=''):
-    """Split lines at newline char, then return the array of stripped lines"""
-    # used e.g. to separate out all the steps in a scenario
-    string = unicode(string)
-    lines = [unicode(l.strip()) for l in string.splitlines()]
-    if ignore_lines_starting_with:
-        filter_func = lambda x: x and not x.startswith(
-            ignore_lines_starting_with)
-    else:
-        # by using an "identity" filter function, blank lines
-        # will not be included in the returned list
-        filter_func = lambda x: x
+    for row in table[1:]:
+        lengths = map(max, zip(lengths, [len(cell) for cell in row]))
 
-    lines = filter(filter_func, lines)
-
-    return lines
-
-
-def split_wisely(string, sep, strip=False):
-    string = unicode(string)
-    if strip:
-        string = string.strip()
-    else:
-        string = string.strip("\n")
-    sep = unicode(sep)
-
-    regex = re.compile(escape_if_necessary(sep),  re.UNICODE | re.M | re.I)
-
-    items = filter(lambda x: x, regex.split(string))
-    if strip:
-        items = [i.strip() for i in items]
-    else:
-        items = [i.strip("\n") for i in items]
-
-    return [unicode(i) for i in items]
-
-
-def wise_startswith(string, seed):
-    string = unicode(string).strip()
-    seed = unicode(seed)
-    regex = u"^%s" % re.escape(seed)
-    return bool(re.search(regex, string, re.I))
-
-
-def remove_it(string, what):
-    return unicode(re.sub(unicode(what), "", unicode(string)).strip())
-
-
-def column_width(string):
-    l = 0
-    for c in unicode(string):
-        if unicodedata.east_asian_width(c) in "WF":
-            l += 2
-        else:
-            l += 1
-    return l
-
-
-def rfill(string, times, char=u" ", append=u""):
-    string = unicode(string)
-    missing = times - column_width(string)
-    for x in range(missing):
-        string += char
-
-    return unicode(string) + unicode(append)
-
-
-def getlen(string):
-    return column_width(unicode(string)) + 1
-
-
-def dicts_to_string(dicts, order):
-    '''
-    Makes dictionary ready for comparison to strings
-    '''
-    escape = "#{%s}" % unicode(time.time())
-
-    def enline(line):
-        return unicode(line).replace("|", escape)
-
-    def deline(line):
-        return line.replace(escape, '\\|')
-
-    keys_and_sizes = dict([(k, getlen(k)) for k in dicts[0].keys()])
-    for key in keys_and_sizes:
-        for data in dicts:
-            current_size = keys_and_sizes[key]
-            value = unicode(data.get(key, ''))
-            size = getlen(value)
-            if size > current_size:
-                keys_and_sizes[key] = size
-
-    names = []
-    for key in order:
-        size = keys_and_sizes[key]
-        name = u" %s" % rfill(key, size)
-        names.append(enline(name))
-
-    table = [u"|%s|" % "|".join(names)]
-    for data in dicts:
-        names = []
-        for key in order:
-            value = data.get(key, '')
-            size = keys_and_sizes[key]
-            names.append(enline(u" %s" % rfill(value, size)))
-
-        table.append(u"|%s|" % "|".join(names))
-
-    return deline(u"\n".join(table) + u"\n")
-
-
-def parse_hashes(lines, json_format=None):
-    escape = "#{%s}" % unicode(time.time())
-
-    def enline(line):
-        return unicode(line.replace("\\|", escape)).strip()
-
-    def deline(line):
-        return line.replace(escape, '|')
-
-    def discard_comments(lines):
-        return [line for line in lines if not line.startswith('#')]
-
-    lines = discard_comments(lines)
-    lines = map(enline, lines)
-
-    keys = []
-    hashes = []
-    if lines:
-        first_line = lines.pop(0)
-        keys = split_wisely(first_line, u"|", True)
-        keys = map(deline, keys)
-
-        for line in lines:
-            values = split_wisely(line, u"|", True)
-            values = map(deline, values)
-            hashes.append(dict(zip(keys, values)))
-
-    return keys, hashes
-
-def json_to_string(json_list, order):
-    '''
-    This is for aesthetic reasons, it will get the width of the largest column and
-    rfill the rest with spaces
-    '''
-    escape = "#{%s}" % unicode(time.time())
-
-    def enline(line):
-        return unicode(line).replace("|", escape)
-
-    def deline(line):
-        return line.replace(escape, '\\|')
-
-    nu_keys_and_sizes = list([[k.keys()[0], getlen(k.keys()[0])] for k in json_list])
-    maxlen = 0
-    for key_list in nu_keys_and_sizes:
-        current_size = key_list[1]
-        counter = 0
-        temp_list = json_list[counter].values()[0]
-        temp_maxlen = len(temp_list)
-        if temp_maxlen > maxlen:
-            maxlen = temp_maxlen
-        for data in temp_list:
-            value = unicode(data)
-            size = getlen(value)
-            if size > current_size:
-                key_list[1] = size
-        counter += 1
-    names = []
-    idx = 0
-    for key in nu_keys_and_sizes:
-        size = key[1]
-        name = u" %s" % rfill(key[0], size)
-        names.append(enline(name))
-
-    table = [u"|%s|" % "|".join(names)]
-
-    for idx in xrange(maxlen):
-        names = []
-        for data, key in zip(json_list, nu_keys_and_sizes):
-            try:
-                value = data.values()[0][idx]
-            except IndexError:
-                value = ''
-            size = key[1]
-            names.append(enline(u" %s" % rfill(value, size)))
-        table.append(u"|%s|" % "|".join(names))
-
-    return deline(u"\n".join(table) + u"\n")
-
-
-def parse_as_json(lines):
-    '''
-        Parse lines into json objects
-    '''
-    escape = "#{%s}" % unicode(time.time())
-    def enline(line):
-        return unicode(line.replace("\\|", escape)).strip()
-
-    def deline(line):
-        return line.replace(escape, '|')
-
-    def discard_comments(lines):
-        return [line for line in lines if not line.startswith('#')]
-    lines = discard_comments(lines)
-    lines = map(enline, lines)
-    non_unique_keys = []
-    json_map = []
-    if lines:
-        first_line = lines.pop(0)
-        non_unique_keys = split_wisely(first_line, u"|", True)
-        non_unique_keys = map(deline, non_unique_keys)
-        rng_idx = len(non_unique_keys)
-        json_map = list(non_unique_keys)
-        for idx in xrange(rng_idx):
-            json_map[idx] = dict([(non_unique_keys[idx], [])])
-        for line in lines:
-            values = split_wisely(line, u"|", True)
-            values = map(deline, values)
-
-            for idx in xrange(rng_idx):
-                json_map[idx].values()[0].append(values[idx])
-    return non_unique_keys, json_map
-
-
-def parse_multiline(lines):
-    multilines = []
-    in_multiline = False
-    for line in lines:
-        if line == '"""':
-            in_multiline = not in_multiline
-        elif in_multiline:
-            if line.startswith('"'):
-                line = line[1:]
-            if line.endswith('"'):
-                line = line[:-1]
-            multilines.append(line)
-    return u'\n'.join(multilines)
+    return u'\n'.join(
+        u' ' * indent +
+        u'| %s |' % u' | '.join(cell.ljust(length)
+                                for cell, length in zip(row, lengths))
+        for row in table
+    )
--- tests/unit/test_strings.py
+++ tests/unit/test_strings.py
@@ -16,314 +16,64 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from nose.tools import assert_equals
+
 from lettuce import strings
 
-def test_escape_if_necessary_escapes_1_char():
-    "strings.escape_if_necessary escapes regex if has only one char"
-    assert_equals(strings.escape_if_necessary("$"), "[$]")
-    assert_equals(strings.escape_if_necessary("^"), "[^]")
-    assert_equals(strings.escape_if_necessary("#"), "[#]")
-    assert_equals(strings.escape_if_necessary("("), "[(]")
-    assert_equals(strings.escape_if_necessary(")"), "[)]")
-    assert_equals(strings.escape_if_necessary("{"), "[{]")
-    assert_equals(strings.escape_if_necessary("}"), "[}]")
 
-def test_escape_if_necessary_escapes_nothing_if_has_more_than_1_char():
-    "Escape if necessary does nothing if the string has more than 1 char"
-    assert_equals(strings.escape_if_necessary("NOT ESCAPED"), "NOT ESCAPED")
+def test_represent_table():
+    """
+    Test representing a table
+    """
 
-def test_get_stripped_lines():
-    "strings.get_stripped_lines strip every line, and jump empty ones"
-    my_string = '''
-
-             first line
-
-       second line
-
-    '''
-    assert_equals(
-        strings.get_stripped_lines(my_string),
-        [
-            'first line',
-            'second line'
-        ]
-    )
-
-def test_get_stripped_lines_ignore_comments():
-    "strings.get_stripped_lines ignore lines that start with some char"
-    my_string = '''
-
-             first line
-
-        # second line
-
-    '''
-    assert_equals(
-        strings.get_stripped_lines(my_string, ignore_lines_starting_with="#"),
-        [
-            'first line',
-        ]
-    )
-
-def test_split_wisely_splits_ignoring_case():
-    "strings.split_wisely splits ignoring case"
-    my_string = 'first line\n' \
-        'second Line\n' \
-        'third LIne\n' \
-        'fourth lINe\n'
-
-    assert_equals(
-        strings.split_wisely(my_string, 'line', strip=False),
-        [
-            'first ',
-            'second ',
-            'third ',
-            'fourth '
-        ]
-    )
-
-def test_split_wisely_splits_ignoring_case_and_stripping():
-    "strings.split_wisely splits ignoring case and stripping"
-    my_string = '''
-
-             first line
-
-       second Line
-
-           third LIne
-       fourth lINe
-
-    '''
-    assert_equals(
-        strings.split_wisely(my_string, 'line', strip=True),
-        [
-            'first',
-            'second',
-            'third',
-            'fourth'
-        ]
-    )
-
-def test_wise_startswith_ignores_case():
-    "strings.wise_startswith ignores case"
-    assert strings.wise_startswith("Gabriel", "g")
-    assert strings.wise_startswith("Gabriel", "G")
-    assert strings.wise_startswith("'Gabriel", "'")
-    assert strings.wise_startswith("#Gabriel", "#")
-    assert strings.wise_startswith("$Gabriel", "$")
-    assert strings.wise_startswith("^Gabriel", "^")
-
-def test_wise_startswith_also_strips_the_string():
-    "strings.wise_startswith ignores case"
-    assert strings.wise_startswith("  Gabriel", "g")
-    assert strings.wise_startswith("  Gabriel", "G")
-    assert strings.wise_startswith("  'Gabriel", "'")
-    assert strings.wise_startswith("  #Gabriel", "#")
-    assert strings.wise_startswith("  $Gabriel", "$")
-    assert strings.wise_startswith("  ^Gabriel", "^")
-
-def test_remove_it_accepts_regex_to_remove_all_from_string():
-    "strings.remove_it accepts regex and remove all matches from string"
-    assert_equals(
-        strings.remove_it(u"Gabriel Falcão", u"[aã]"),
-        "Gbriel Flco"
-    )
-
-def test_column_width():
-    "strings.column_width"
-    assert_equals(
-        strings.column_width(u"あいうえお"),
-        10
-    )
-
-def test_column_width_w_number_and_char():
-    "strings.column_width_w_number_and_char"
-    assert_equals(
-        strings.column_width( u"%s%c" % (u"4209", 0x4209)),
-        6
-    )
-    
-def test_rfill_simple():
-    "strings.rfill simple case"
-    assert_equals(
-        strings.rfill("ab", 10, "-"),
-        "ab--------"
-    )
-
-def test_rfill_empty():
-    "strings.rfill empty"
-    assert_equals(
-        strings.rfill("", 10, "*"),
-        "**********"
-    )
-
-def test_rfill_blank():
-    "strings.rfill blank"
-    assert_equals(
-        strings.rfill(" ", 10, "|"),
-        " |||||||||"
-    )
-
-def test_rfill_full():
-    "strings.rfill full"
-    assert_equals(
-        strings.rfill("abcdefghij", 10, "|"),
-        "abcdefghij"
-    )
-
-def test_rfill_append():
-    "strings.rfill append"
-    assert_equals(
-        strings.rfill("ab", 10, append="# path/to/file.extension: 2"),
-        "ab        # path/to/file.extension: 2"
-    )
-
-def test_dicts_to_string():
-    "strings.dicts_to_string"
-
-    dicts = [
-        {
-            'name': u'Gabriel Falcão',
-            'age': 22
-        },
-        {
-            'name': 'Miguel',
-            'age': 19
-        }
-
+    table = [
+        ['name', 'age'],
+        [u'Gabriel Falcão', 22],
+        ['Miguel', 19],
     ]
 
     assert_equals(
-        strings.dicts_to_string(dicts, ['name', 'age']),
+        strings.represent_table(table),
         u"| name           | age |\n"
         u"| Gabriel Falcão | 22  |\n"
-        u"| Miguel         | 19  |\n"
+        u"| Miguel         | 19  |"
     )
 
-def test_dicts_to_string_escapes_pipe():
-    "strings.dicts_to_string escapes pipe"
 
-    dicts = [
-        {
-            'name': u'Gabriel | Falcão',
-            'age': 22
-        },
-        {
-            'name': 'Miguel | Arcanjo',
-            'age': 19
-        }
+def test_represent_table_escapes_pipe():
+    """
+    Test representing a table with escaping
+    """
 
+    table = [
+        ['name', 'age'],
+        [u'Gabriel | Falcão', 22],
+        ['Miguel | Arcanjo', 19],
     ]
 
     assert_equals(
-        strings.dicts_to_string(dicts, ['name', 'age']),
-        u"| name             | age |\n"
-        u"| Gabriel \\| Falcão | 22  |\n"
-        u"| Miguel \\| Arcanjo | 19  |\n"
+        strings.represent_table(table),
+        ur"| name              | age |" "\n"
+        ur"| Gabriel \| Falcão | 22  |" "\n"
+        ur"| Miguel \| Arcanjo | 19  |"
     )
 
-def test_dicts_to_string_allows_empty():
-    "strings.dicts_to_string allows empty"
 
-    dicts = [
-        {
-            'name': u'Gabriel | Falcão',
-            'age': 22
-        },
-        {
-            'name': 'Miguel | Arcanjo'
-        }
+def test_represent_table_allows_empty():
+    """
+    Test representing a table with an empty cell
+    """
+
+    table = [
+        ['name', 'age'],
+        [u'Gabriel | Falcão', 22],
+        ['Miguel | Arcanjo', ''],
     ]
+
+    print strings.represent_table(table)
+
     assert_equals(
-        strings.dicts_to_string(dicts, ['name', 'age']),
-        u"| name             | age |\n"
-        u"| Gabriel \\| Falcão | 22  |\n"
-        u"| Miguel \\| Arcanjo |     |\n"
+        strings.represent_table(table),
+        ur"| name              | age |" "\n"
+        ur"| Gabriel \| Falcão | 22  |" "\n"
+        ur"| Miguel \| Arcanjo |     |"
     )
-
-def test_parse_hashes():
-    "strings.parse_hashes"
-
-    keys = [u'name', u'age']
-    dicts = [
-        {
-            u'name': u'Gabriel Falcão',
-            u'age': u'22'
-        },
-        {
-            u'name': u'Miguel',
-            u'age': u'33'
-        }
-
-    ]
-
-    table = [
-        u"| name           | age |\n",
-        u"| Gabriel Falcão | 22  |\n",
-        u"| Miguel         | 33  |\n",
-    ]
-
-    got_keys, got_dicts = strings.parse_hashes(table)
-
-    assert_equals(keys, got_keys)
-    assert_equals(dicts, got_dicts)
-
-def test_parse_hashes_escapes_pipes():
-    "strings.parse_hashes escapes pipe"
-
-    keys = [u'name', u'age']
-    dicts = [
-        {
-            u'name': u'Gabriel | Falcão',
-            u'age': u'22'
-        },
-        {
-            u'name': u'Miguel | Silva',
-            u'age': u'33'
-        }
-
-    ]
-
-    table = [
-        u"| name              | age |\n",
-        u"| Gabriel \| Falcão | 22  |\n",
-        u"| Miguel \| Silva   | 33  |\n",
-    ]
-
-    got_keys, got_dicts = strings.parse_hashes(table)
-
-    assert_equals(keys, got_keys)
-    assert_equals(dicts, got_dicts)
-
-
-def test_parse_hashes_allow_empty():
-    "strings.parse_hashes allow empty"
-
-    keys = [u'name', u'age']
-    dicts = [
-        {
-            u'name': u'Gabriel',
-            u'age': u'22'
-        },
-        {
-            u'name': u'',
-            u'age': u'33'
-        },
-        {
-            u'name': u'Dave',
-            u'age': u''
-        }
-
-    ]
-
-    table = [
-        u"| name    | age |\n",
-        u"| Gabriel | 22  |\n",
-        u"|         | 33  |\n",
-        u"| Dave    |     |\n",
-    ]
-
-    got_keys, got_dicts = strings.parse_hashes(table)
-
-    assert_equals(keys, got_keys)
-    assert_equals(dicts, got_dicts)
