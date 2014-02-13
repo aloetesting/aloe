46264eee4656a795c0324e587342676782e3455b
Artur Daschevici <a.daschevici@gmail.com>
2014-02-13 14:43:06 +0200

    changed also to and, it caused test to fail

--- tests/functional/simple_features/3rd_feature_dir/my_steps_are_anywhere.feature
+++ tests/functional/simple_features/3rd_feature_dir/my_steps_are_anywhere.feature
@@ -5,5 +5,5 @@ Feature: Having step definition all along folders
   Scenario: Looking for steps anywhere
     Given I define step at look/here/step_one.py
     And at look/and_here/step_two.py
-    Also at look/here/for_steps/step_three.py
+    And at look/here/for_steps/step_three.py
     And finally at look/and_here/and_any_python_file/step_four.py
--- tests/functional/test_step_definition_loading.py
+++ tests/functional/test_step_definition_loading.py
@@ -48,7 +48,7 @@ def test_recursive_fallback():
         [
             'Given I define step at look/here/step_one.py',
             'And at look/and_here/step_two.py',
-            'Also at look/here/for_steps/step_three.py',
+            'And at look/here/for_steps/step_three.py',
             'And finally at look/and_here/and_any_python_file/step_four.py',
         ]
     )
