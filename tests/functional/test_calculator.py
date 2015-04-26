c52a086dfed39e9a4c49737b26733988a8ff18f6
Alexey Kotlyarov <a@koterpillar.com>
2015-04-26 20:10:03 +1000

    Simplify the usage of in_directory

--- tests/functional/test_calculator.py
+++ tests/functional/test_calculator.py
@@ -21,9 +21,13 @@ from __future__ import absolute_import
 from future import standard_library
 standard_library.install_aliases()
 
-from . import FeatureTest
+from . import (
+    FeatureTest,
+    in_directory,
+)
 
 
+@in_directory('tests/tested_app')
 class CalculatorTest(FeatureTest):
     """
     Test that calculator feature works as expected.
@@ -34,42 +38,37 @@ class CalculatorTest(FeatureTest):
         Test running the calculator feature.
         """
 
-        with self.in_directory('tests/tested_app'):
-            result = self.run_features('features/calculator.feature')
-            assert result.success
+        result = self.run_features('features/calculator.feature')
+        assert result.success
 
     def test_wrong_expectations(self):
         """
         Test that a failing feature fails tests.
         """
 
-        with self.in_directory('tests/tested_app'):
-            result = self.run_features('features/wrong_expectations.feature')
-            assert not result.success
+        result = self.run_features('features/wrong_expectations.feature')
+        assert not result.success
 
     def test_background(self):
         """
         Test running a scenario with a background.
         """
 
-        with self.in_directory('tests/tested_app'):
-            result = self.run_features('features/background.feature')
-            assert result.success
+        result = self.run_features('features/background.feature')
+        assert result.success
 
     def test_outlines(self):
         """
         Test a scenario with outlines.
         """
 
-        with self.in_directory('tests/tested_app'):
-            result = self.run_features('features/outlines.feature')
-            assert result.success
+        result = self.run_features('features/outlines.feature')
+        assert result.success
 
     def test_python_test_skipped(self):
         """
         Test that the Python test does not get picked up.
         """
 
-        with self.in_directory('tests/tested_app'):
-            result = self.run_features('tests')
-            assert result.success
+        result = self.run_features('tests')
+        assert result.success
