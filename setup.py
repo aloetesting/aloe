4b4c6d73a3e6f67c82ee76eb1a5b820d569b54cd
Alexey Kotlyarov <a@koterpillar.com>
2015-05-13 21:19:23 +1000

    Version bump

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open('requirements.txt') as requirements, \
         open('test_requirements.txt') as test_requirements:
     setup(
         name='aloe',
-        version='0.0.1',
+        version='0.0.2',
         description='Gherkin runner compatible with Lettuce',
         author='Alexey Kotlyarov',
         author_email='a@koterpillar.com',
