7c2231e4d8c87a1fafb51dc746cc6b815466fbc4
Alexey Kotlyarov <a@koterpillar.com>
2015-06-05 08:48:00 +1000

    Only load features from package directories

--- aloe/plugin.py
+++ aloe/plugin.py
@@ -59,7 +59,7 @@ class GherkinPlugin(Plugin):
 
         self.feature_dirs = [
             os.path.abspath(dir_)
-            for dir_ in FeatureLoader.locate('.', 'features')
+            for dir_ in FeatureLoader.find_feature_directories('.')
         ]
         for feature_dir in self.feature_dirs:
             FeatureLoader.find_and_load_step_definitions(feature_dir)
