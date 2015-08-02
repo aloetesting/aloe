8ba737752917e18dbe53e97b3cc1d124f42a0193
Alexey Kotlyarov <a@koterpillar.com>
2015-08-02 11:52:10 +1000

    Add missing __init__

--- tests/unit/features/steps.py
+++ tests/unit/features/steps.py
@@ -14,6 +14,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+"""
+Steps for features used in unit tests.
+"""
+
 from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import division
