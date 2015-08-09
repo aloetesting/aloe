ce30118e0140c8bff39602dacf29b02bbd523ac2
Danielle Madeley <dmadeley@infoxchange.net.au>
2015-08-09 15:39:41 +1000

    Capture nose output to StringIO

--- tests/functional/test_outputter.py
+++ tests/functional/test_outputter.py
@@ -26,6 +26,7 @@ from builtins import *
 from future import standard_library
 standard_library.install_aliases()
 
+from io import StringIO
 from aloe.testing import (
     FeatureTest,
     in_directory,
@@ -38,10 +39,75 @@ class OutputterTest(FeatureTest):
     Test level 3 outputter
     """
 
-    def test_outputter(self):
-        """
-        Test Factory Boy steps.
-        """
+    maxDiff = None
 
-        self.run_features('features/highlighting.feature',
-                          verbosity=3)
+    def test_output(self):
+        """Test streamed output"""
+
+        with StringIO() as stream:
+            self.run_features('features/highlighting.feature',
+                              verbosity=3, stream=stream)
+
+            print("--Output--")
+            print(stream.getvalue())
+            print("--END--")
+
+            # FIXME: where do these right alignments come from?
+            self.assertEqual(stream.getvalue(), """
+Feature: Scenario indices
+
+  As a programmer
+  I want to see my scenarios with pretty highlighting
+  So my output is easy to read
+
+  Scenario: First scenario                                                              features/highlighting.feature:9
+    Given I have entered 10 into the calculator
+    And I press add
+
+  Scenario Outline: Scenario outline with two examples                                  features/highlighting.feature:13
+      | number |
+      | 30     |
+
+    Given I have entered <number> into the calculator
+    And I press add
+
+  Scenario Outline: Scenario outline with two examples                                  features/highlighting.feature:13
+      | number |
+      | 40     |
+
+    Given I have entered <number> into the calculator
+    And I press add
+
+  Scenario: Scenario with table                                                         features/highlighting.feature:22
+    Given I press add:
+      | value |
+      | 1     |
+      | 1     |
+      | 2     |
+
+----------------------------------------------------------------------
+Ran 3 tests in 0.003s
+
+OK
+""".lstrip())
+
+    def test_color_output(self):
+        """Test streamed output with color"""
+
+        with StringIO() as stream:
+            self.run_features('features/highlighting.feature',
+                              verbosity=3, stream=stream,
+                              force_color=True)
+
+            print("--Output--")
+            print(stream.getvalue())
+            print("--END--")
+
+            self.assertEqual(stream.getvalue(), """
+Feature: Scenario indices
+
+  As a programmer
+  I want to see my scenarios with pretty highlighting
+  So my output is easy to read
+
+""".lstrip())
