a29a36cb716be1b06b7b1f9f43ddf83a19589f8e
Danielle Madeley <dmadeley@infoxchange.net.au>
2015-08-09 16:12:11 +1000

    Write a mock for colored output

--- tests/functional/test_outputter.py
+++ tests/functional/test_outputter.py
@@ -26,11 +26,48 @@ from builtins import *
 from future import standard_library
 standard_library.install_aliases()
 
+from unittest.mock import patch
+
 from io import StringIO
 from aloe.testing import (
     FeatureTest,
     in_directory,
 )
+
+
+class TermElement(object):
+    def __init__(self, attr):
+        self.attr = attr
+
+    def __call__(self, s):
+        # strips the string to make the output easier to read
+        return 't.{attr}({s})'.format(attr=self.attr, s=s.strip())
+
+    def __str__(self):
+        return '<t.{attr}>'.format(attr=self.attr)
+
+    def __mul__(self, other):
+        return str(self) * other
+
+
+class MockTerminal(object):
+
+    def __init__(self, stream, force_styling=None):
+        self.stream = stream
+        self.does_styling = True
+
+    def __getattr__(self, attr):
+        if attr in ('stream',
+                    'does_styling',
+                    '__getstate__'):
+            raise AttributeError(attr)
+
+        # callable factory
+        return TermElement(attr)
+
+    def color(self, color):
+
+        return getattr(self, 'color%s' % color)
 
 
 @in_directory('tests/simple_app')
@@ -44,7 +81,9 @@ class OutputterTest(FeatureTest):
     def test_output(self):
         """Test streamed output"""
 
-        with StringIO() as stream:
+        with \
+                StringIO() as stream, \
+                patch('aloe.result.AloeTestResult.printSummary'):
             self.run_features('features/highlighting.feature',
                               verbosity=3, stream=stream)
 
@@ -85,16 +124,15 @@ Feature: Scenario indices
       | 1     |
       | 2     |
 
-----------------------------------------------------------------------
-Ran 3 tests in 0.003s
-
-OK
 """.lstrip())
 
     def test_color_output(self):
         """Test streamed output with color"""
 
-        with StringIO() as stream:
+        with \
+                patch('aloe.result.Terminal', new=MockTerminal), \
+                patch('aloe.result.AloeTestResult.printSummary'), \
+                StringIO() as stream:
             self.run_features('features/highlighting.feature',
                               verbosity=3, stream=stream,
                               force_color=True)
@@ -104,10 +142,57 @@ OK
             print("--END--")
 
             self.assertEqual(stream.getvalue(), """
-Feature: Scenario indices
+t.bold_white(Feature: Scenario indices)
 
-  As a programmer
+t.white(As a programmer
   I want to see my scenarios with pretty highlighting
-  So my output is easy to read
+  So my output is easy to read)
+
+t.bold_white(Scenario: First scenario)t.color8(features/highlighting.feature:9)
+t.color8(Given I have entered 10 into the calculator
+    And I press add)
+<t.move_up><t.move_up>t.color11(Given I have entered 10 into the calculator)
+<t.move_up>t.bold_green(Given I have entered 10 into the calculator)
+t.color11(And I press add)
+<t.move_up>t.bold_green(And I press add)
+
+t.bold_white(Scenario Outline: Scenario outline with two examples)t.color8(features/highlighting.feature:13)
+      | t.white(number) |
+      | t.white(30) |
+
+t.color8(Given I have entered <number> into the calculator
+    And I press add)
+<t.move_up><t.move_up>t.color11(Given I have entered <number> into the calculator)
+<t.move_up>t.bold_green(Given I have entered <number> into the calculator)
+t.color11(And I press add)
+<t.move_up>t.bold_green(And I press add)
+
+t.bold_white(Scenario Outline: Scenario outline with two examples)t.color8(features/highlighting.feature:13)
+      | t.white(number) |
+      | t.white(40) |
+
+t.color8(Given I have entered <number> into the calculator
+    And I press add)
+<t.move_up><t.move_up>t.color11(Given I have entered <number> into the calculator)
+<t.move_up>t.bold_green(Given I have entered <number> into the calculator)
+t.color11(And I press add)
+<t.move_up>t.bold_green(And I press add)
+
+t.bold_white(Scenario: Scenario with table)t.color8(features/highlighting.feature:22)
+t.color8(Given I press add:
+      | value |
+      | 1     |
+      | 1     |
+      | 2     |)
+<t.move_up><t.move_up><t.move_up><t.move_up><t.move_up>t.color11(Given I press add:)
+      | t.color11(value) |
+      | t.color11(1) |
+      | t.color11(1) |
+      | t.color11(2) |
+<t.move_up><t.move_up><t.move_up><t.move_up><t.move_up>t.bold_green(Given I press add:)
+      | t.bold_green(value) |
+      | t.bold_green(1) |
+      | t.bold_green(1) |
+      | t.bold_green(2) |
 
 """.lstrip())
