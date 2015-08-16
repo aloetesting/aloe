9c32a597275fa200fe6a4453cabd5a49657620da
Alexey Kotlyarov <a@koterpillar.com>
2015-08-16 18:12:15 +1000

    Remove individual copyright headers

--- tests/functional/test_calculator.py
+++ tests/functional/test_calculator.py
@@ -1,19 +1,3 @@
-# Aloe - Cucumber runner for Python based on Lettuce and Nose
-# Copyright (C) <2015> Alexey Kotlyarov <a@koterpillar.com>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 """
 Basic scenario tests.
 """
@@ -76,7 +60,7 @@ class CalculatorTest(FeatureTest):
         self.assertIn(feature_stack_frame, output)
 
         step_stack_frame = """
-  File "{root}/tests/simple_app/features/steps.py", line 76, in assert_result
+  File "{root}/tests/simple_app/features/steps.py", line 60, in assert_result
     assert world.result == float(result)
 AssertionError
         """.strip().format(root=dirname(dirname(dirname(__file__))))
