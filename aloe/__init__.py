9d33fc7f2fa46b0aaa2c3dc948b80275e2e49af2
Marius van Niekerk <marius.v.niekerk@gmail.com>
2017-05-21 12:09:15 -0400

    Added MANIFEST.in to ensure that licence is included in distribution

--- aloe/__init__.py
+++ aloe/__init__.py
@@ -36,3 +36,7 @@ def main(argv=None):  # pragma: no cover
 
 if __name__ == '__main__':  # pragma: no cover
     main()
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
